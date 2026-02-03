1b3cf9c9ea367e6d28f147ba326e69b239aaa178
Steven Leonardo <stevenleonardolim@gmail.com>
2026-02-03 15:42:52 +0700

    set visibility header mobile

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [menu, setMenu] = useState(false)
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 0);
@@ -12,9 +13,29 @@ export default function Header() {
 
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+  const openMenu = () => {
+    setMenu(true)
+  };
+  const closeMenu = () => {
+    setMenu(false)
+  };
   return (
     <div className={`header_cmp ${isScrolled ? "scroll" : ""} component`}>
       <div className="header_cmp--page-list">
+        <Image
+          className={`close-menu ${menu ? "show" : "hide"}`}
+          src="/close.png"
+          width={1000}
+          height={1000}
+          onClick={closeMenu}
+        />
+        <Image
+          className={`open-menu ${menu ? "hide" : "show"}`}
+          src="/burger.png"
+          width={1000}
+          height={1000}
+          onClick={openMenu}
+        />
         <ul>
           <li>
             <Link href="/">Home</Link>
@@ -33,33 +54,21 @@ export default function Header() {
       <div className="header_cmp--logo-list">
         <ul>
           <li>
-            <Link href="https://www.linkedin.com/in/steven-leonardo-lim" target="_blank">
-              <Image
-                width="600"
-                height="500"
-                src="/linkedin.png"
-                alt="test"
-              />
+            <Link
+              href="https://www.linkedin.com/in/steven-leonardo-lim"
+              target="_blank"
+            >
+              <Image width="600" height="500" src="/linkedin.png" alt="test" />
             </Link>
           </li>
           <li>
             <Link href="https://github.com/Fullaby" target="_blank">
-              <Image
-                width="600"
-                height="500"
-                src="/github.png"
-                alt="test"
-              />
+              <Image width="600" height="500" src="/github.png" alt="test" />
             </Link>
           </li>
           <li>
             <Link href="mailto:stevenleonardo57@gmail.com">
-              <Image
-                width="600"
-                height="500"
-                src="/emaillogo.png"
-                alt="test"
-              />
+              <Image width="600" height="500" src="/emaillogo.png" alt="test" />
             </Link>
           </li>
         </ul>
